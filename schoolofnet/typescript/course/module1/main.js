fb83520a5ed84a91d724a602f6fb46b030aa622b
Denis sales <denisales@hotmail.com>
2018-11-29 18:36:35 -0200

    finalizando

---
+++ schoolofnet/typescript/course/module1/main.js
@@ -0,0 +1,10 @@
+var App;
+(function (App) {
+    var Car = /** @class */ (function () {
+        function Car() {
+        }
+        return Car;
+    }());
+    var mustang = new Car();
+    console.log(mustang);
+})(App || (App = {}));
