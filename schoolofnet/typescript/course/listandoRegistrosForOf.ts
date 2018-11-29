4ac98a4df5081e7182c7cab180043bc0b8fc7afe
Denis sales <denisales@hotmail.com>
2018-11-29 17:32:57 -0200

    listando registros com for of

---
+++ schoolofnet/typescript/course/listandoRegistrosForOf.ts
@@ -0,0 +1,22 @@
+var listCar = ["Camaro","Ferrari","Porche"];
+
+for(var i in listCar){
+    console.log(i)
+}
+
+for (var car of listCar){
+    console.log(car)
+}
+
+var colors = new Array(["Red", "Blue", "Orange"]);
+
+colors['title'] = 'Colors'
+
+console.log(colors)
+
+for(var i in colors){
+    console.log(i)
+}
+for (var color of colors){
+    console.log(color)
+}
